5cbfddd1fdd3b9294e915116dbfed064ad6c648b
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-09-26 17:53:09 -0400

    Update main.go

--- api/main.go
+++ api/main.go
@@ -5,11 +5,17 @@ import (
 	"log"
 
 	"github.com/gmvbr/todo-go/api/config"
+	"github.com/gmvbr/todo-go/api/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	env := config.LoadEnv()
+
+	db, err := database.ConnectDB(env.MongoUrl, env.MongoDb)
+	if err != nil {
+		log.Fatal(err)
+	}
 	
     app := fiber.New()
 	url := fmt.Sprintf(":%s", env.Port)
